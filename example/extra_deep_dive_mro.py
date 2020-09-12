1f17a4adbf7c6a023f735c1d5441c561b5d8f97a
Robert Westin <70424136+robert-alfwar@users.noreply.github.com>
2020-09-12 23:40:55 +0200

    added mro example

---
+++ example/extra_deep_dive_mro.py
@@ -0,0 +1,22 @@
+class Base:
+    def __init__(self):
+        print(self.__class__.mro())
+
+class X(Base):
+    def method(self):
+        print("X")
+
+class Y(Base):
+    def method(self):
+        print("Y")
+
+class A(Y):
+    pass
+
+class B(A):
+    pass
+
+class C(B, X):
+    pass
+
+C().method()
